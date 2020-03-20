aa9fb675ce4fc89ef29cf1744ae9964a71577d23
Mervin Pan <mervinpan@gmail.com>
2020-03-20 11:32:37 -0700

    Added database seed file

---
+++ sample_data/seed.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/fec', {useNewUrlParser: true, useUnifiedTopology: true });
+const fs = require('fs');
+
+var ProductSchema = new Schema ({
+  productNumber: { type: Number, required: true },
+  productName: String,
+  productDescription: String,
+  productCategory: String,
+})
+
+var ProductModel = mongoose.model('products', ProductSchema);
+
+var data = JSON.parse(fs.readFileSync(__dirname + '/sample_products.json', 'utf8'))
+
+ProductModel.insertMany(data, function(error, docs) {
+  if (error) {
+    console.log('Error seeding database');
+  } else {
+    console.log('Successfully seeded database');
+  }
+})
+
